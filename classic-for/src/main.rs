85372df70765b2380625064098e30f6831426f7d
DumbMahreeo <massi.noviello@gmail.com>
2021-10-21 21:35:07 +0200

    Adding classic for macro

---
+++ classic-for/src/main.rs
@@ -0,0 +1,40 @@
+macro_rules! cfor {
+    (($stmt:stmt; $cond:expr; $counter:ident++) $code:block) => {
+        $stmt
+        while $cond {
+            $code;
+            $counter+=1;
+        }
+    };
+
+    (($stmt:stmt; $cond:expr; $counter:ident--) $code:block) => {
+        $stmt
+        while $cond {
+            $code;
+            $counter-=1;
+        }
+    };
+
+    (($stmt:stmt; $cond:expr; $incr:stmt) $code:block) => {
+        $stmt
+        while $cond {
+            $code;
+            $incr
+        }
+    };
+
+}
+
+fn main() {
+    cfor!{
+
+        (let mut a = 1; a < 10; a++) {
+            println!("{}", a);
+        }
+
+
+    }
+}
+
+
+
